d1561d43ad2b3128ce7e19fd714d18fde99cb314
張百寬 <quan@dhcp-9-240-191-236.c3t-tw.ibmmobiledemo.com>
2023-07-26 10:45:23 +0800

    update

--- lib/models/repo/error_report.dart
+++ lib/models/repo/error_report.dart
@@ -1,3 +1,9 @@
+
+// Error report model for electricity consumption dashboard
+// Path: lib/models/repo/error_report.dart
+// 每分鐘更新資料
+import 'package:flutter/material.dart';
+
 class ErrorReportModel{
   final ElectricityConsumptionErrorType? errorType;
   final String? errorDescription;
@@ -20,6 +26,7 @@ class ErrorReportModel{
     this.volt,
     this.power
   });
+  // from json
   factory ErrorReportModel.fromJson(Map<String, dynamic> json){
     return ErrorReportModel(
       errorType: json['errorType'],
@@ -33,9 +40,49 @@ class ErrorReportModel{
       power: json['power']
     );
   }
+
+  // to Json
+  Map<String, dynamic> toJson() => {
+    'errorType': errorType,
+    'errorDescription': errorDescription,
+    'startTime': startTime,
+    'loc': loc,
+    'building': building,
+    'tagId': tagId,
+    'ampere': ampere,
+    'volt': volt,
+    'power': power
+  };
+
 }
 
 enum ElectricityConsumptionErrorType{
-  sharplyIncrease, sharplyDecrease, abnormal, other;
-  
+  sharplyIncrease(
+    label: "徒增",
+    iconsColor: Colors.red,
+    icon: Icons.arrow_upward
+  ),
+  sharplyDecrease(
+    label: "突降",
+    iconsColor: Colors.orange,
+    icon: Icons.arrow_downward
+  ),
+  abnormal(
+    label: "異常",
+    iconsColor: Colors.purple,
+    icon: Icons.error
+  ),
+  other(
+    label: "其他",
+    iconsColor: Colors.amber,
+    icon: Icons.error
+  );
+  const ElectricityConsumptionErrorType({
+    required this.label,
+    this.iconsColor = Colors.red,
+    this.icon = Icons.error
+  });
+  final String label;
+  final Color iconsColor;
+  final IconData icon;
 }
